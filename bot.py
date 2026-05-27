e289835b7ae6f2988405446423bd207013cd2f99
secretniibreinrot-debug <secretniibreinrot@gmail.com>
2026-05-27 22:31:09 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,14 @@
 import telebot
 from telebot import types
 
-# Твій робочий токен
+# Твій токен
 bot = telebot.TeleBot("8803136202:AAGQkKSMzLcExYMHUl-fpQ1GWNzQudo2VU8")
 
 @bot.message_handler(commands=['start'])
 def start_command(message):
     markup = types.InlineKeyboardMarkup()
     
-    # ВИПРАВЛЕНО: Додано точну назву твого репозиторію "privid-app"
+    # Твоє точне посилання на сайт
     web_app_info = types.WebAppInfo(url="https://secretniibreinrot-debug.github.io/privid-app/")
     btn_site = types.InlineKeyboardButton(text="👻 Відкрити ПРИВІД", web_app=web_app_info)
     
